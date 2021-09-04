1708b7d41243b4dcb3ab1555475891ad50fbcfdd
Mengyan Zhang <u6015325@anu.edu.au>
2021-09-04 22:36:22 +0800

    add yml file; add f3

--- run_sim.py
+++ run_sim.py
@@ -12,11 +12,11 @@ import pretty_errors
 #-----------------------------------------------------------------------------------
 # parameters
 
-save_folder = 'GPOO_results2/'
+save_folder = 'GPOO_results3/'
 
 # np.random.seed(2021)
 n = 80 # budget
-n_repeat = 1 # number of repeat
+n_repeat = 100 # number of repeat
 arms_range = [0.0, 1.0] # root cell
 reward_type = 'center' # 'center' or 'ave'
 sigma = 0.1 # noise for observation (normal std)
@@ -24,8 +24,8 @@ k = 2 # k-ary tree
 s = 10 # split for aggregated feedback
 d = 1 # feature dim
 
-lengthscale = 0.1 # kernel para
-kernel_var = 0.5 # kernel para
+lengthscale = 0.05 # kernel para
+kernel_var = 0.1 # kernel para
 gp_noise_var = kernel_var * 0.05 # 1e-10 # gp para
 opt_flag = False # whether optimise parameters in gpr
 
@@ -36,6 +36,12 @@ run_GPTree = True
 run_random = True
 plot_regret = True
 plot_tree = True
+# run_StoOO = False
+# run_GPOO = False
+# run_GPTree = False
+# run_random = False
+# plot_regret = True
+# plot_tree = True
 
 # ----------------------------------------------------------------------------------
 # plot regret from saved file
@@ -44,7 +50,7 @@ if plot_regret:
     regret_dict = {}
 
     for alg in ['GPOO', 'StoOO', 'Random', 'GPTreee']:
-        saved_file = alg + '_regret_' + str(n) + '_' + str(n_repeat) + '.pickle'
+        saved_file = save_folder + alg + '_regret_' + str(n) + '_' + str(n_repeat) + '.pickle'
 
         if os.path.isfile(saved_file):
             with open(saved_file, 'rb') as handle:
@@ -81,9 +87,31 @@ def f(x):
     # Y = np.array([0.85, 0.1, 0.87, 0.05, 0.98]).reshape(sample_size,1)
 
     # option 2:
+    # sample_size = 9
+    # X = np.array([0.05, 0.2, 0.3, 0.4, 0.5, 0.65, 0.75, 0.9, 0.95]).reshape(sample_size,1)
+    # Y = np.array([0.1, 0.3, 0.15, 0.35, 0.12, 0.85, 0.05, 0.98, 0.3]).reshape(sample_size,1)
+
+    # option 3:
+    np.random.seed(2021)
     sample_size = 9
-    X = np.array([0.05, 0.2, 0.3, 0.4, 0.5, 0.65, 0.75, 0.9, 0.95]).reshape(sample_size,1)
-    Y = np.array([0.1, 0.3, 0.15, 0.35, 0.12, 0.85, 0.05, 0.98, 0.3]).reshape(sample_size,1)
+    X = []
+    Y = []
+    split_list = np.linspace(arms_range[0], arms_range[1], num = 9)
+    for i in range(len(split_list)-1):
+        center = (split_list[i] + split_list[i+1])/2.0
+        y = np.random.uniform(0.0, 0.2)
+        X.append(center)
+        Y.append(y) # each center is assigned to a value 0~0.2
+
+        another = np.random.uniform(split_list[i], split_list[i+1])
+        X.append(another)
+        Y.append(np.random.uniform(0.4, 0.6))
+
+    X.append(0.9)
+    Y.append(0.9)
+    X = np.array(X).reshape(-1, 1)
+    Y = np.array(Y).reshape(-1,1)
+    # plt.scatter(X,Y)
 
     kernel = GPy.kern.RBF(input_dim=d, 
                         variance=kernel_var, 
